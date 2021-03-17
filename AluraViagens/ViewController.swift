34f1f77209611932ded324a6031e1ed2c2bb2fd8
Renilson <renilson.moreiraf@gmail.com>
2021-03-17 09:53:27 -0300

    Desenvolvimento do Header

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    
+    let listaViagens: Array<String> = ["Rio de Janeiro", "Ceara", "São Paulo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tabelaViagens.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listaViagens[indexPath.row]
+        return cell
     }
 
 
